fe17f671e38f0e4295e34b3b1e2462655c2684ff
sorfal <sorfal.web@gmail.com>
2015-09-24 12:00:35 +0700

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,48 @@
+var app = {
+
+  initialize: function() {
+    this.bindEvents();
+  },
+
+  bindEvents: function() {
+    this.blockScreen = app.blockScreen();
+    document.addEventListener('deviceready', this.onDeviceReady, false)
+    document.addEventListener("online", this.onOnline.bind(this), false);
+    document.addEventListener("offline", this.onOffline.bind(this), false);
+  },
+
+  onDeviceReady: function() {
+    console.log('onDeviceReady');
+  },
+
+  onOnline: function() {
+    console.log('onOnline');
+    this.blockScreen(true);
+  },
+
+  onOffline: function() {
+    console.log('onOffline');
+    this.blockScreen(false);
+  },
+
+  blockScreen: function() {
+    var
+      InAppBrowser;
+
+    return function(show) {
+      if (show) {
+        console.log(InAppBrowser);
+        if (InAppBrowser) {
+          InAppBrowser = null;
+        }
+        InAppBrowser = cordova.InAppBrowser.open('http://fatenation.ru/', '_blank', 'location=no,toolbar=no');
+      } else {
+        if (InAppBrowser) {
+          InAppBrowser.close();
+        }
+      }
+    }
+  }
+};
+
+app.initialize();
